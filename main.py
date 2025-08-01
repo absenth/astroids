d066d9363eeddb76b209c55f79a2dbb1e2010cc6
absenth <1115138+absenth@users.noreply.github.com>
2025-07-31 20:27:57 -0500

    adding game clock

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ from constants import *
 
 
 def main():
+    dt = 0
+
     pygame.init()
+    Clock = pygame.time.Clock()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -14,8 +17,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+
             screen.fill("black")
             pygame.display.flip()
+            tick_value = Clock.tick(60)
+            dt = tick_value / 1000
+
 
 
 if __name__ == "__main__":
